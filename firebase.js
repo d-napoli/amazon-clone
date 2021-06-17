42ceaf486447aab0772885f67d1930bae277c9ee
Diego Napoli <37269543+d-napoli@users.noreply.github.com>
2021-06-17 20:59:11 -0300

    Added cart page

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAVpg3Ksw102yAzcvS_FVnYZEUjzZijs6M",
+    authDomain: "clone-react-cae3e.firebaseapp.com",
+    projectId: "clone-react-cae3e",
+    storageBucket: "clone-react-cae3e.appspot.com",
+    messagingSenderId: "345800492888",
+    appId: "1:345800492888:web:18c556757961eb72d2c72d"
+  };
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,17 @@ import {
     ShoppingCartIcon
 } from "@heroicons/react/outline"
 
+import { signIn, signOut, useSession } from "next-auth/client"
+import { useRouter } from "next/router"
+import { useSelector } from "react-redux"
+import { selectItems } from "../slices/basketSlice"
+
 function Header() {
+    const [session] = useSession()
+    const router = useRouter()
+
+    const items = useSelector(selectItems)
+
     return (
         <header>
             {/* Top nav */}
@@ -15,6 +25,7 @@ function Header() {
                 {/* Logo */}
                 <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
                     <Image
+                        onClick={() => router.push('/')}
                         src="https://links.papareact.com/f90"
                         width={150}
                         height={40}
@@ -32,8 +43,10 @@ function Header() {
                 {/* Right Section, cart, hello, etc */}
                 <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
                     {/* Hello */}
-                    <div className="link">
-                        <p>Hello Diego Napoli</p>
+                    <div onClick={!session ? signIn : signOut} className="cursor-pointer link">
+                        <p className="hover:underline">
+                            {session ? `Hello, ${session.user.name}` : "Sign In"}
+                        </p>
                         <p className="font-extrabold md:text-sm">Account & Lists</p>
                     </div>
 
@@ -44,9 +57,9 @@ function Header() {
                     </div>
 
                     {/* Cart */}
-                    <div className="relative link flex items-center">
+                    <div onClick={() => router.push("/checkout")} className="relative link flex items-center">
                         {/* Number above the basket */}
-                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">{items.length}</span>
 
                         <ShoppingCartIcon className="h-10" />
                         <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
